9093520a9f0448bf29b00f2d2b2a2cdbfd137c65
Josh Kellendonk <joshkellendonk@gmail.com>
2022-12-18 15:15:05 -0700

    mobile menu

--- packages/web/src/components/MainLayout.tsx
+++ packages/web/src/components/MainLayout.tsx
@@ -4,15 +4,15 @@ import clsx from 'clsx';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import logo from '../assets/logo-mark.svg';
+import Image from 'next/image';
 
-export const MainLayout: React.FC<React.PropsWithChildren<{}>> = ({
-  children,
-}) => {
+export const MainLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
   const NavTextLink = (props: React.PropsWithChildren<{ href: string }>) => (
     <Link
       href={props.href}
       className={clsx([
-        'rounded-lg py-1 px-2 text-lg text-slate-700 hover:bg-slate-100 hover:text-slate-900 flex items-center gap-2',
+        'flex items-center gap-1',
+        'rounded-lg py-1 px-2 text-slate-700 hover:bg-slate-100 hover:text-slate-900 text-sm',
       ])}
     >
       {props.children}
@@ -25,28 +25,19 @@ export const MainLayout: React.FC<React.PropsWithChildren<{}>> = ({
         <header className="py-5">
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <nav className="relative z-50 flex justify-between">
-              <div className="hidden md:flex md:gap-x-5">
-                <Link aria-label="Home" href="/" className="flex items-center mr-10">
-                  <img
-                    src={logo}
-                    width="50"
-                    height="50"
-                    alt="The Letter K"
-                  />
-                  for Kellendonk
-                </Link>
+              <Link aria-label="Home" href="/" className="flex items-center">
+                <Image src={logo} width="50" height="50" alt="The Letter K" />
+                <span className="hidden sm:inline">for Kellendonk</span>
+              </Link>
 
-
-              </div>
-
-              <div className="flex items-center gap-x-5 md:gap-x-8">
+              <div className="flex flex-col">
                 <NavTextLink href="https://github.com/misterjoshua">
                   <FontAwesomeIcon icon={faGithub} width={20} />
-                  @misterjoshua
+                  misterjoshua
                 </NavTextLink>
                 <NavTextLink href="https://github.com/lineape">
                   <FontAwesomeIcon icon={faGithub} width={20} />
-                  @lineape
+                  lineape
                 </NavTextLink>
               </div>
             </nav>
