86499deef99f354291b928f58a2718757967a63e
Josh Kellendonk <joshkellendonk@gmail.com>
2022-12-18 15:24:00 -0700

    front page content

--- packages/web/src/components/HeroSection.tsx
+++ packages/web/src/components/HeroSection.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
-export const HeroSection: React.FC<React.PropsWithChildren<{}>> = ({ children }) => (
+export const HeroSection: React.FC<React.PropsWithChildren> = ({
+  children,
+}) => (
   <section>
     <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-20 text-center lg:py-32">
-      <h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
-        {children}
-      </h1>
+      {children}
     </div>
   </section>
+);
+
+export const HeroTitle: React.FC<React.PropsWithChildren> = ({ children }) => (
+  <h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
+    {children}
+  </h1>
 );
--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { MainLayout } from '../components/MainLayout';
-import { HeroSection } from '../components/HeroSection';
+import { HeroSection, HeroTitle } from '../components/HeroSection';
 
 export function Page() {
   return (
     <MainLayout>
       <HeroSection>
-        Home page for{' '}
-        <span className="text-[#ED3125] sm:whitespace-nowrap">many things</span>
+        <HeroTitle>
+          Home page for{' '}
+          <span className="text-[#ED3125] sm:whitespace-nowrap">
+            many things
+          </span>
+        </HeroTitle>
+
+        <p className="text-xl py-14">Though, none of it is here yet. Check back later.</p>
       </HeroSection>
     </MainLayout>
   );
