b77b8833ab04595771910263e4f757af85372e63
Josh Kellendonk <joshkellendonk@gmail.com>
2022-12-19 16:10:16 -0700

    add google analytics

--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 
 import './_app.css';
+import Script from 'next/script';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
@@ -9,6 +10,21 @@ function CustomApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Welcome to web!</title>
       </Head>
+
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-JMCDB5JZXK"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-JMCDB5JZXK');
+        `}
+      </Script>
+
       <main className="app">
         <Component {...pageProps} />
       </main>
