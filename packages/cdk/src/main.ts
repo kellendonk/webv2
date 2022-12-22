906e2d425468ed14dfdb5126cc6c5369cfc091f8
Josh Kellendonk <joshkellendonk@gmail.com>
2022-12-22 16:44:53 -0700

    wipe out the test stage

--- packages/cdk/src/main.ts
+++ packages/cdk/src/main.ts
@@ -5,14 +5,6 @@ const app = new App();
 
 new WebStage(app, 'Kellendonk-Dev');
 
-new WebStage(app, 'Kellendonk-Test', {
-  domain: {
-    domainName: 'www-test.kellendonk.ca',
-    hostedZoneId: 'Z04480822SF8LKAO9VKJ5',
-    hostedZoneName: 'kellendonk.ca',
-  },
-});
-
 new WebStage(app, 'Kellendonk-Production', {
   domain: {
     domainName: 'www.kellendonk.ca',
