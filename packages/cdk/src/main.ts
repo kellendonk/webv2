05cc3923d9c114a83b3c911fba3467b3127a7353
Josh Kellendonk <joshkellendonk@gmail.com>
2022-12-30 17:23:34 -0700

    change identity domain to id.kellendonk.c.a

--- packages/cdk/src/main.ts
+++ packages/cdk/src/main.ts
@@ -17,7 +17,7 @@ new KellendonkStage(app, 'Kellendonk-Production', {
     ...kellendonkCa,
   },
   identityDomainName: {
-    domainName: 'auth.kellendonk.ca',
+    domainName: 'id.kellendonk.ca',
     ...kellendonkCa,
   },
 });
